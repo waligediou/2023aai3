afa6cc0d30c5bb511b9544bbac1acc6bf769a577
waligediou <144758452+waligediou@users.noreply.github.com>
2024-10-24 16:32:35 +0800

    Add files via upload

---
+++ week07/week07-4.cpp
@@ -0,0 +1,12 @@
+// week07-4.cpp
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s);
+        string word;
+        while( ss >> word ){
+
+        }
+        return word.length();
+    }
+};
