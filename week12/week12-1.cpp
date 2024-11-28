f371f86dbd58815e2b490f47547075022317407f
waligediou <144758452+waligediou@users.noreply.github.com>
2024-11-28 16:11:51 +0800

    Add files via upload

---
+++ week12/week12-1.cpp
@@ -0,0 +1,18 @@
+/// week12-1.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+struct Node {
+    int val;
+};
+class Cat {
+public:
+    string name;
+};
+int main()
+{
+    Cat cat1;
+    cat1.name = "Kitty";
+    Node node;
+    node.val = 1;
+}
---
+++ week12/week12-2.cpp
@@ -0,0 +1,21 @@
+// week12-2.cpp
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        //ListNode* ans=list1;
+        //ListNode* ans=new ListNode(99);
+        ListNode* ans=new ListNode(99,new ListNode(88));
+
+        return ans;
+    }
+};
