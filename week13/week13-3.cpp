57e4533a61b0bdbe05735d9ebd875f38a89a5c10
waligediou <144758452+waligediou@users.noreply.github.com>
2024-12-05 16:41:01 +0800

    Add files via upload

---
+++ week13/week13-3.cpp
@@ -0,0 +1,12 @@
+// week13-3.cpp
+int main() {
+    vector<int> A, B;
+    int a, b;
+    while( cin >> a >> b ) {
+        A.push_back(a);
+        B.push_back(b);
+    }
+    for(int i=0; i<A.size(); i++) {
+        cout << A[i] << " ";
+    }    
+}
