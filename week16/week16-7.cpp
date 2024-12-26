32e61211841b1064a1fc8e887b58890c6a41c197
waligediou <144758452+waligediou@users.noreply.github.com>
2024-12-26 16:13:52 +0800

    Add files via upload

---
+++ week16/week16-7.cpp
@@ -0,0 +1,20 @@
+// week16-7.cpp
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int T, N;
+	cin >> T;
+	for(int t=0; t<T; t++){
+		cin >> N;
+		vector<int> a(N);
+		for(int i=0; i<N; i++){
+			cin >> a[i];
+		}
+		
+		
+		cout << "Optimal train swapping takes 1 swaps.\n";
+	}
+}			
