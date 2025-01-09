93b62859ad54bee925d10c658d7f4f9096028609
waligediou <144758452+waligediou@users.noreply.github.com>
2025-01-09 15:29:13 +0800

    week18

---
+++ week18/week18-1a.cpp
@@ -0,0 +1,18 @@
+// week18-1a.cpp
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int ans = 0;
+        for(string word : words) {
+            int bad = 0;
+            for(int i=0; pref[i] !=0; i++) {
+                if(pref[i] != word[i]) { 
+                    bad = 1;
+                    break;
+                }    
+            }    
+            if(bad==0) ans++;
+        }
+        return ans;       
+    }
+};
